bb8c87825d31a6263f5d3d2cab33c3790166d45a
shofny <sho.fuyuyado0705@gmail.com>
2020-09-24 19:33:05 +0900

    deviseに関するビューファイルの配置

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,6 +21,7 @@ Devise.setup do |config|
   # config.parent_controller = 'DeviseController'
 
   # ==> Mailer Configuration
+  # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
